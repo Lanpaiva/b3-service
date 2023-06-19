0f70ac3d768c56e38affef24a21a92332478a8f5
Allan <allanpaiva17@gmail.com>
2023-06-19 18:35:31 -0300

    feat: market, transactions, orders, investor and book added

---
+++ internal/market/entity/book.go
@@ -0,0 +1,91 @@
+package entity
+
+import (
+	"container/heap"
+	"sync"
+)
+
+type Book struct {
+	Order            []*Order
+	Transaction      []*Transaction
+	OrdersChannel    chan *Order
+	OrdersChannelOut chan *Order
+	Wg               *sync.WaitGroup
+}
+
+func NewBook(orderChannel chan *Order, orderChannelOut chan *Order, wg *sync.WaitGroup) *Book {
+	return &Book{
+		Order:            []*Order{},
+		Transaction:      []*Transaction{},
+		OrdersChannel:    orderChannel,
+		OrdersChannelOut: orderChannelOut,
+		Wg:               wg,
+	}
+}
+
+func (b *Book) Trade() {
+	buyOrders := NewOrderQueue()
+	sellOrders := NewOrderQueue()
+
+	heap.Init(buyOrders)
+	heap.Init(sellOrders)
+
+	for order := range b.OrdersChannel {
+		if order.OrderType == "BUY" {
+			buyOrders.Push(order)
+			if sellOrders.Len() > 0 && sellOrders.Orders[0].Price <= order.Price {
+				sellOrder := sellOrders.Pop().(*Order)
+				if sellOrder.PendingShares > 0 {
+					transaction := NewTransaction(sellOrder, order, order.Shares, sellOrder.Price)
+					b.AddTransaction(transaction, b.Wg)
+					sellOrder.Transactions = append(sellOrder.Transactions, transaction)
+					b.OrdersChannelOut <- sellOrder
+					b.OrdersChannelOut <- order
+					if sellOrder.PendingShares > 0 {
+						sellOrders.Push(sellOrder)
+					}
+				}
+			}
+		} else if order.OrderType == "SELL" {
+			sellOrders.Push(order)
+			if buyOrders.Len() > 0 && buyOrders.Orders[0].Price >= order.Price {
+				buyOrder := buyOrders.Pop().(*Order)
+				if buyOrder.PendingShares > 0 {
+					transaction := NewTransaction(order, buyOrder, order.Shares, buyOrder.Price)
+					b.AddTransaction(transaction, b.Wg)
+					buyOrder.Transactions = append(buyOrder.Transactions, transaction)
+					order.Transactions = append(order.Transactions, transaction)
+					b.OrdersChannel <- buyOrder
+					b.OrdersChannelOut <- order
+					if buyOrder.PendingShares > 0 {
+						buyOrders.Push(buyOrder)
+					}
+				}
+			}
+		}
+	}
+}
+
+func (b *Book) AddTransaction(transaction *Transaction, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	sellingShares := transaction.SellingOrder.PendingShares
+	buyingShares := transaction.BuyingOrder.PendingShares
+
+	minShares := sellingShares
+	if buyingShares < minShares {
+		minShares = buyingShares
+	}
+	transaction.SellingOrder.Investor.UpdateAssetPosition(transaction.SellingOrder.Asset.ID, -minShares)
+	transaction.AddSellOrderPendingShares(-minShares)
+
+	transaction.BuyingOrder.Investor.UpdateAssetPosition(transaction.BuyingOrder.Asset.ID, minShares)
+	transaction.AddBuyOrderPendingShares(-minShares)
+
+	transaction.CalculateTotal(transaction.Shares, transaction.BuyingOrder.Price)
+
+	transaction.CloseBuyOrder()
+	transaction.CloseSellOrder()
+
+	b.Transaction = append(b.Transaction, transaction)
+}
